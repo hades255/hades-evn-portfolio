40b600f8af5c171f409abd314471058f1527b202
hades255 <hades.255@outlook.com>
2023-08-17 06:22:54 +0900

    layouts, scroll to top

---
+++ public/javascripts/scrollTop.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  "use strict";
+  $(window).on("scroll", function () {
+    if ($(this).scrollTop() > 600) {
+      $(".return-to-top").fadeIn();
+    } else {
+      $(".return-to-top").fadeOut();
+    }
+  });
+  $(".return-to-top").on("click", function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      1500
+    );
+    return false;
+  });
+});
