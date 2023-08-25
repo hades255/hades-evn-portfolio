a8e56eedb27679798858f92b5512521f1ec53c53
hades255 <hades.255@outlook.com>
2023-08-25 20:57:23 +0900

    new repository form

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,16 @@
-var express = require("express");
-const { create, apicontacts,setAsRead, setAsImportant } = require("../controllers/contact");
+const express = require("express");
+
+const multer = require("multer");
+const upload = multer({ dest: "public/uploads/" });
+
+const {
+  create,
+  apicontacts,
+  setAsRead,
+  setAsImportant,
+} = require("../controllers/contact");
 const { signup, signin } = require("../controllers/auth");
-var router = express.Router();
+const router = express.Router();
 
 /* GET users listing. */
 router.post("/signup", signup);
@@ -9,7 +18,11 @@ router.post("/signin", signin);
 
 router.post("/contact", create);
 router.get("/admin/contacts", apicontacts);
-router.post("/admin/contacts/:id/read", setAsRead)
-router.post("/admin/contacts/:id/mark", setAsImportant)
+router.post("/admin/contacts/:id/read", setAsRead);
+router.post("/admin/contacts/:id/mark", setAsImportant);
+
+router.post("/upload", upload.single("file"), (req, res, next) => {
+  res.json({ file: req.file.filename });
+});
 
 module.exports = router;
